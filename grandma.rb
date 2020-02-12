1e27af5615c7eeea3f58f95f95d91019701c466a
Jen Miao <jenmiao@gmail.com>
2020-02-12 15:29:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if !(phrase.upcase)
-    return "HUH?! SPEAK UP, SONNY!"
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
 end
 
 # def speak_to_grandma(phrase = "I LOVE GRANDMA!")
